87f2a5dde8003d42c25bed75f4103678d5b1b575
Pedro Rodrigues <medecau@gmail.com>
2023-09-20 11:26:24 +0200

    Fix issues with tool evals file
    Some evals were causing issues because compinit was not loaded,
    this is now fixed.
    
    Added autocompletion for the llm tool, this will help with
    autocompletion of template names.

--- .zshrc.d/01_evals.zsh
+++ .zshrc.d/01_evals.zsh
@@ -1,6 +1,11 @@
-[ -f ~/.hidden.zsh ] && source ~/.hidden.zsh
+autoload -Uz compinit
+compinit
+
+eval "$(_LLM_COMPLETE=zsh_source llm)"
 
 eval "$(direnv hook zsh)"
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+[ -f ~/.hidden.zsh ] && source ~/.hidden.zsh
 
 eval "$(starship init zsh)"
