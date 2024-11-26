5dacd9591151673193e04afa4ee961eb3a21ad72
Pedro Rodrigues <medecau@gmail.com>
2024-11-26 16:43:51 +0100

    fix compression for packed git objets add some context comments
    add another telemetry opt-out
    add env var for the java home
    
    add libomp for openmp
    add netnewswire cask

--- .zshrc.d/00_exports.zsh
+++ .zshrc.d/00_exports.zsh
@@ -12,6 +12,7 @@ export SAM_CLI_TELEMETRY=0            # AWS Serverless Application Model
 export AZURE_CORE_COLLECT_TELEMETRY=0 # Azure CLI
 export MEILI_NO_ANALYTICS=1           # MeiliSearch
 export MEILI_NO_SENTRY=1
+export OTEL_SDK_DISABLED=1            # opentelemetry - crewai
 export SOURCEBOT_TELEMETRY_DISABLED=1              # SOURCEBOT
 export NEXT_PUBLIC_SOURCEBOT_TELEMETRY_DISABLED=1
 # semgrep metrics are disabled in alias
@@ -88,6 +89,8 @@ if [ -z "$TMUX" ]; then # global environment
     export PATH="$PATH:$(brew --prefix rust)/bin"
     export PATH="$PATH:$HOME/.cargo/bin"
     export PATH="$PATH:$(brew --prefix ruby)/bin"
+
+    export JAVA_HOME=$(/usr/libexec/java_home)
 fi
 
 export PATH="$HOME/bin:$HOME/.local/bin:$PATH"
