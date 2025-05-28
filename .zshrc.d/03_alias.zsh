22323db73ef3ba91e7d96c3e6e0cbcff709b7ff0
Pedro Rodrigues <me@pdbr.org>
2025-05-28 15:45:55 +0200

    Increase thinking tokens limit for Claude alias
    Set MAX_THINKING_TOKENS=20000 for the cl alias to enable
    extended reasoning capabilities in Claude interactions

--- .zshrc.d/03_alias.zsh
+++ .zshrc.d/03_alias.zsh
@@ -54,7 +54,7 @@ alias rg='rg \
     --colors=line:fg:121 \
     --colors=path:fg:183'
 
-alias cl=claude
+alias cl='MAX_THINKING_TOKENS=20000 claude'
 
 alias myip='hget https://icanhazip.com'
 alias myipv4='hget https://ipv4.icanhazip.com'
